a470671df886117cc79b16b10b1dd6b1a91b81f9
itsmearafik <arafikalhassan@gmail.com>
2024-08-21 19:51:17 +0100

    basics of python using learnxinyminutes guide

---
+++ datatypes.py
@@ -0,0 +1,40 @@
+""" 
+This is the Begiining of the Learn X in Y minutes for python repo
+"""
+
+# 1. Primitive Datatypes and Operations 
+
+# Numbers 
+1 + 1 
+10 - 2 
+
+39 / 3  # the result for division is always float 
+
+# floor division 
+5 // 3  # 1
+-5 // 3  # -2
+5.0 // 3.0 # 1.0 
+
+# Modulo operation 
+7 % 3 # 1
+
+# i % j have the same sign as j
+-7 % 3 # 2
+
+# Exponentiation (x**y, x to the yth power)
+2**3  # 8
+
+# Enforce precedence with parentheses
+1 + 3 * 2  # 7
+
+(1 + 3) * 2  # 8
+
+# Boolean values are primitives (Note: the capitalizations)
+True 
+False 
+
+# negate with not 
+not True  # False 
+not False # True 
+
+# Note "and" & "or" are case-sensitive 
