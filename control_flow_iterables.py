a26b5de217ab2af875b7cf98060a1c58d4c3838d
itsmearafik <arafikalhassan@gmail.com>
2024-10-27 13:10:30 +0000

    Practiced the use of the  python keyword

--- control_flow_iterables.py
+++ control_flow_iterables.py
@@ -12,12 +12,37 @@ else:
     print("some_var is indeed 10.")
 
 
+print("______end of line______")
+
 """ For loops iterate over lists """
 mammals = ["dog", "cat", "mouse"]
 
 for animal in mammals:
     print("{} is a mammal".format(animal))
 
+print("______end of line______")
 
 for cart in ["washing soap", "detergent","sponge", "hand gloves"]:
     print("{} is part of your shop items".format(cart).capitalize())
+
+
+print("______end of line______")
+# `range(number)` returns an iterable of numbers from zero up to (excluding) the given number
+
+for i in range(4):
+    print(i)
+
+print("______end of line______")
+# `range(lower, upper)` returns an iterable from the lower number to the upper number 
+
+for i in range(4,8):
+    print(i)
+
+print("______end of line______")
+# `range(lower, upper, step)` returns an iterable of numbers from the lower number to the upper number, while incrementing by step.The default step value is 1 if its not indicated
+
+for i in range(4,8,2):
+    print(i)
+
+print("______end of line______")
+
