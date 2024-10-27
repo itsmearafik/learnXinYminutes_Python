e0f7f9e90bb97073926bf2a7b3a24818a87c1d4d
itsmearafik <arafikalhassan@gmail.com>
2024-10-27 12:52:50 +0000

    Practiced the use of the  python keyword

---
+++ control_flow_iterables.py
@@ -0,0 +1,13 @@
+"""Control Flow and Iterables"""
+
+some_var = 5
+
+# for conditionals, functions, etc the convention is to use four spaces not tabs 
+
+if some_var > 10:
+    print("some_var is totally bigger than 10.")
+elif some_var < 10:
+    print("some_var is smaller than 10.")
+else:
+    print("some_var is indeed 10.")
+
