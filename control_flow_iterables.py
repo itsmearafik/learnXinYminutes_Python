29624cf1907a100eed672bcd44a83a6ec5bcc46e
itsmearafik <arafikalhassan@gmail.com>
2024-10-27 22:12:54 +0000

    Practiced the use of iterators and the  python keyword.

--- control_flow_iterables.py
+++ control_flow_iterables.py
@@ -89,6 +89,90 @@ finally: # it executes under all circumstances
 
 
 # You can use `with` stetement intead of try/finally to cleanup resources 
-with open("myfile.txt") as f:
+with open("./myfile.txt") as f:
     for line in f:
         print(line)
+
+# Writing to a file, Working with files 
+contents = {"aa": 12, "bb": 21}
+with open("./myfile1.txt", "w") as file:
+    file.write(str(contents)) # writes a string to a files
+    file.write("My name is arafik")
+
+import json 
+with open("myfile2.txt", "w") as file:
+    file.write(json.dumps(contents))
+
+print("______end of line______")
+    
+# Reading from a file 
+with open("./myfile1.txt") as file:
+    contents = file.read()
+
+print(contents)
+
+print("______end of line______")
+
+with open("./myfile2.txt", "r") as file:
+    contents = json.load(file)
+print(contents)
+
+
+print("______end of line______")
+
+# An iterable is an object that can be treated as a sequence
+
+filled_dict = {"one": 1, "two": 2, "three": 3}
+our_iterable = filled_dict.keys()
+print(our_iterable) # This returns an object that implements an iterable interface
+
+print("______end of line______")
+
+# looping over iterables 
+for i in our_iterable:
+    print(i) # Prints one, two, three [ iterabls ]
+
+
+print("______end of line______")
+
+# Iterables cannot be accessed by the index method 
+try:
+    our_iterable[1]
+except:
+    print("Iterables elements cannot be addressed by the index")
+
+
+print("______end of line______")
+
+# An iterable is an object that can create an iterator 
+our_iterator = iter(our_iterable)
+
+print("______end of line______")
+
+# Iterators is an object that can remember the state as we traverse through
+# You can get the next object of an iterator with the `next()` keyword 
+
+next(our_iterator)
+
+print("______end of line______")
+next(our_iterator)
+
+print("______end of line______")
+next(our_iterator)
+# The iterator raises a StopIteration exception after it has returned all of its data. 
+
+print("______end of line______")
+# Using the for loop to loop over an iterable 
+# The `for` keyword loops over the iterable implicitly 
+our_iterator = iter(our_iterable)
+
+for i in our_iterator:
+    print(i)
+
+
+print("______end of line______")
+# You can grab all the elements of an iterable or iterator by call of a list()
+list(our_iterable)
+
+print("______end of line______")
+list(our_iterator)
